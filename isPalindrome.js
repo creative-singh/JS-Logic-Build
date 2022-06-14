2884e32b5624c37e929c5541e2f1b11d49e821eb
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2022-06-15 01:12:57 +0530

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,17 +1,17 @@
-function toCheckPalindrome(inputArr){
-    let halfLength = inputArr.length  2;
+function toCheckPalindrome(inputArr) {
+    let halfLength = inputArr.length / 2;
     let length = inputArr.length;
-    
-    for(let i = 0; i  halfLength; i++){
+
+    for (let i = 0; i < halfLength; i++) {
         let swappedVar = length - i - 1;
-        if(inputArr[i] !== inputArr[swappedVar]){
-            return Not a Palindrome;
+        if (inputArr[i] !== inputArr[swappedVar]) {
+            return "Not a Palindrome";
         }
-    }
-    return Palindrome;
+    }   
+    return "Palindrome";
 }
 
 
- let input = [1,2,3,2,1];
-let input = tata;
+let input = [1, 2, 3, 2, 1];
+// let input = tata;
 console.log(toCheckPalindrome(input));
