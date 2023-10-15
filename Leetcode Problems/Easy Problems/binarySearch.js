ec074df996bd815d45044e8bafc3303d893891be
Bhavleen Singh <singhbhavleen3@gmail.com>
2023-10-15 17:52:00 +0530

    Binary Search in Leetcode

---
+++ Leetcode Problems/Easy Problems/binarySearch.js
@@ -0,0 +1,22 @@
+// Question Link: https://leetcode.com/problems/binary-search
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (target === nums[middle]) return middle
+    if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return -1;
+};
