83b8fed1bcaa86740ef8dc1fa02aab9678b5b0e8
Bhavleen Singh <singhbhavleen3@gmail.com>
2023-11-15 13:07:00 +0530

    Tiny URL/ URL Shortner

---
+++ Leetcode Problems/Medium Problems/tinyUrlDecodeEncode.js
@@ -0,0 +1,25 @@
+// Question Link: https://leetcode.com/problems/encode-and-decode-tinyurl/
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function (longUrl) {
+  return longUrl
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function (shortUrl) {
+  return shortUrl
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
