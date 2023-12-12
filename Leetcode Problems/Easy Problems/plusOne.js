645dd1ca78bc51e0313b68f1ff2b000bcb27f192
creative-singh <singhbhavleen3@gmail.com>
2023-12-12 18:44:29 +0530

    Plus One Medium Problem Solved

--- Leetcode Problems/Easy Problems/plusOne.js
+++ Leetcode Problems/Easy Problems/plusOne.js
@@ -1,19 +1,38 @@
 // Question Link: https://leetcode.com/problems/plus-one
-
 var plusOne = function (digits) {
   let add = 0;
   let plusOne = 1;
-  for (let i = digits.length - 1; i >= 0; i--) {
-    add = digits[i] + plusOne + add;
+  let ind = digits.length - 1;
+  while (ind >= 0) {
+    add = digits[ind] + plusOne + add;
     if (add < 10) {
-      digits.splice(i, 1, add);
+      digits.splice(ind, 1, add);
       return digits;
     } else {
-      digits.splice(i, 1, add % 10);
+      digits.splice(ind, 1, add % 10);
       add = Math.floor(add / 10);
       plusOne = 0;
     }
+    ind--;
   }
-  if (add) digits.unshift(add);
+  if (add) digits = [add, ...digits];
   return digits;
 };
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] = digits[i] + 1;
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3])); // [1,2,4]
+console.log(plusOne([4, 3, 2, 1])); // [4,3,2,2]
+console.log(plusOne([9])); // [1,0]
+console.log(plusOne([9, 9])); // [1,0,0]
