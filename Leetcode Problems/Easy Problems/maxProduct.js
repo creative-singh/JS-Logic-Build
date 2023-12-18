4d5b6b47df085c1f900a06dadbfc0c865031ccfe
creative-singh <singhbhavleen3@gmail.com>
2023-12-18 17:16:30 +0530

    Leetcode Daily Challenge

---
+++ Leetcode Problems/Easy Problems/maxProduct.js
@@ -0,0 +1,9 @@
+// Question Link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+
+var maxProductDifference = function (nums) {
+  nums.sort((a, b) => a - b);
+  return (
+    nums[nums.length - 1] * nums[nums.length - 2] -
+    nums[0] * nums[1]
+  );
+};
