bcc66f3f742dbe6ba05fab99bc00253a695dad62
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2025-04-05 00:57:35 +0530

    Create running-sum.js

---
+++ Leetcode Problems/Easy Problems/running-sum.js
@@ -0,0 +1,8 @@
+// Question Link: https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+var runningSum = function(nums) {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i-1]
+    }   
+    return nums 
+};
