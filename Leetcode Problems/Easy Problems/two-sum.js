b5b4ebfc19214ca3a002322ff9bb84607066b1a6
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2025-04-16 12:02:01 +0530

    More optimised TwoSum

--- Leetcode Problems/Easy Problems/two-sum.js
+++ Leetcode Problems/Easy Problems/two-sum.js
@@ -1,14 +1,21 @@
 // Question Link: https://leetcode.com/problems/two-sum/
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+// O(n^2)
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) return [i, j]
     }
+  }
+};
+
+// O(n)
+var twoSum = function(nums, target) {
+  let map = new Map()
+  map.set(nums[0], 0)
+  for(let i = 1; i < nums.length; i++) {
+      let temp = target - nums[i]
+      if(map.has(temp)) return [map.get(temp), i]
+      else map.set(nums[i], i)
   }
 };
