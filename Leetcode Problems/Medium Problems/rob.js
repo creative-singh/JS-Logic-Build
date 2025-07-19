b0641c15a0d9b8bebfef0f80df68bbb77de88d94
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2025-07-20 01:44:46 +0530

    Medium: House Robber

---
+++ Leetcode Problems/Medium Problems/rob.js
@@ -0,0 +1,21 @@
+// Question Link: https://leetcode.com/problems/house-robber/
+// 198. House Robber
+
+var rob = function(nums, dp = []) {
+    let n = nums.length;
+    let prev1 = nums[0];
+    let prev2 = 0; let curr;
+
+    for(let i =  1; i < n; i++) {
+        let pick = nums[i]
+        if(i > 1) pick += prev2
+
+        let notPick = 0 + prev1
+
+        curr = Math.max(pick, notPick)
+        prev2 = prev1
+        prev1 = curr
+    }
+
+    return prev1
+};
