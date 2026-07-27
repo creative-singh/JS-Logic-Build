9a3b56d7d87b73208e9c50f8ffc810209d03350c
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2026-07-28 00:30:25 +0530

    Easy: Maximum Product of Two Elements in an Array

--- Leetcode Problems/Easy Problems/maxProduct.js
+++ Leetcode Problems/Easy Problems/maxProduct.js
@@ -1,4 +1,5 @@
-// Question Link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+// Question Link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs
+// 1464. Maximum Product of Two Elements in an Array
 
 var maxProductDifference = function (nums) {
   nums.sort((a, b) => a - b);
@@ -7,3 +8,17 @@ var maxProductDifference = function (nums) {
     nums[0] * nums[1]
   );
 };
+
+// TS
+// function maxProduct(nums: number[]): number {
+//     let max = nums[0], sMax = -1
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] > max) {
+//             sMax = max
+//             max = nums[i]
+//         } else if (sMax < nums[i]) {
+//             sMax = nums[i]
+//         }
+//     }
+//     return (max - 1) * (sMax - 1)
+// };
