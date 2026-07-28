288831791817907fef066b78aaeae35f349437ff
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2026-07-29 00:10:15 +0530

    Medium: Smallest Palindromic Rearrangement I

---
+++ Leetcode Problems/Medium Problems/smallestPalindrome.ts
@@ -0,0 +1,19 @@
+// Question Link: https://leetcode.com/problems/smallest-palindromic-rearrangement-i
+// 3517. Smallest Palindromic Rearrangement I
+
+function smallestPalindrome(s: string): string {
+    let freq = Array(26).fill(0);
+
+    for (const c of s)
+        freq[c.charCodeAt(0) - 97]++;
+
+    let left = "", mid = "";
+
+    for (let i = 0; i < 26; i++) {
+        left += String.fromCharCode(97 + i).repeat(Math.floor(freq[i] / 2));
+        if (freq[i] & 1)
+            mid = String.fromCharCode(97 + i);
+    }
+
+    return left + mid + [...left].reverse().join("");
+};
