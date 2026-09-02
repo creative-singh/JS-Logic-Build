d5a259713b7d961af5f4ecaaa6f1ccd1580376bb
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2026-09-03 00:42:34 +0530

    Easy: Construct Uniform Parity Array I

---
+++ Leetcode Problems/Easy Problems/uniformArray.ts
@@ -0,0 +1,6 @@
+// Question Link: https://leetcode.com/problems/construct-uniform-parity-array-i
+// 3875. Construct Uniform Parity Array I
+
+function uniformArray(nums1: number[]): boolean {
+    return true;
+};
